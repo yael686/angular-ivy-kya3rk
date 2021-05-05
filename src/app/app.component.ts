47bc4e8d331b41397566cc91786644fa6f8bab4d
yael686 <yaelchasidim@gmail.com>
2021-05-05 23:57:08 +0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent {
+  title = "flower";
 }
